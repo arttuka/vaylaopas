7d4e9713d6bb7d5bedfcb81646fdba20c165feb7
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2020-08-21 22:25:36 +0300

    Update dependencies

--- src/server/indexHtml.js
+++ src/server/indexHtml.js
@@ -5,7 +5,7 @@ const indexHtml = (cfg, bundle) => `
   <head>
     <meta charset="utf-8" />
     <script crossorigin="anonymous" src="https://polyfill.io/v3/polyfill.min.js?features=Promise"></script>
-    <link rel="stylesheet" href="https://api.tiles.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.css">
+    <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
     <style type="text/css">
       body {
