be03caa82707a12d5960ee12126dd5baa74bfc11
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2021-07-30 13:36:44 +0300

    Load source maps on dev

--- webpack.dev.babel.js
+++ webpack.dev.babel.js
@@ -43,6 +43,11 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
     ],
   },
   output: {
