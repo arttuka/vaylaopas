60e52ea3cecca58e6693d1265cbef6c5d495354f
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2022-03-10 21:46:49 +0200

    Update other dependencies

--- src/client/redux/store.ts
+++ src/client/redux/store.ts
@@ -1,6 +1,6 @@
 import { applyMiddleware, createStore } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from '@redux-devtools/extension'
 import { rootReducer } from './reducers'
 import { rootSaga } from './sagas'
 
